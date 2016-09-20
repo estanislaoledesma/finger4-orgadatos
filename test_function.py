38d9aa9262106815397fd5b2768fd9a9953ad18b
Estanislao <estanislaomledesma@gmail.com>
2016-09-19 23:57:08 -0300

    Función de hash.

--- test_function.py
+++ test_function.py
@@ -1,6 +1,13 @@
 def myhash(s):
-	return hash(s)
-
+    h = 8
+    for i, c in enumerate(s):
+        c = ord(c)
+        h += c * (c + 11923) + (i + 1)
+        h ^= (h << 5)
+        h += (h << 23)
+    h += (h << 5)
+    h ^= (h >> 15)
+    return h
 i=0
 col=0
 noncol = 0
@@ -10,11 +17,11 @@ with open('words.txt') as f:
         i=i+1
         z = myhash(line) % 400000
         if z in d:
-        	col = col + 1
-        	d[z]+=1
+            col = col + 1
+            d[z]+=1
         else:
-        	noncol = noncol+1
-        	d[z]=1
+            noncol = noncol+1
+            d[z]=1
 print "Number of lines in file: "+str(i)
 print "Collisions: " +str(col)
 print "Non Collisions: "+str(noncol)
